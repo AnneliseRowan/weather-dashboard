3dd7bd64b8dcd0f8aa45a9eac71ae5c757367af3
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-03 20:06:33 -0400

    messed with the code, still won't display

--- script.js
+++ script.js
@@ -11,52 +11,29 @@ let citySearched;
 
 const apiKey = "4994e9dae0cfd4d7106550fa9a769e96";
 
-function titleCase(str) {
-    let splitStr = str.toLowerCase().split(" "); 
-    console.log(splitStr)
-    for(var i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1).toLowerCase(); 
-    }
-
-    return splitStr.join(" "); 
-}
-
-
-function formSubmitHandler(event) {
-    event.preventDefault(); 
-
-    let city = titleCase(cityInputEl.value.trim());
-
-    if(city) {
-        getCurrentWeather(city);
-        cityInputEl.value= ""; 
-    } else {
-        alert("Please enter a city!")
-    }
-}
 
 
 function getCurrentWeather(cityName) {
-
     let requestUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+    console.log(requestUrl)
     
-    fetch(requestUrl)
-    .then(function (response) {
-        response.json(); 
-        console.log(response); 
-    }).then(function (data) {
-        displayWeather(data, cityName); 
-        get5DayForecast(cityName); 
-        return data.coord;
-    }).then(function(coord) {
-        getUVindex(coord.lat, coord.lon); 
-    }).catch(function(error) {
-        alert("We have a slight issue");
-        forecastContainerEl.innerHTML = ""; 
+    $.ajax(requestUrl, {
+        dataType: "json",
+        success: function(data) {
+            displayWeather(data, cityName);
+            get5DayForecast(cityName); 
+            getUVindex(data.corrd.lat, data.coord.lon)
+            return data.coord
+        },
+        error: function() {
+            alert("We have a slight issue!");
+            forecastContainerEl.innerHTML = ""; 
+        }
+        
     })
 
 }
-
 
 function displayCurrentWeather(data, cityName) {
     currentWeatherContainerEL.innerHTML = ""; 
@@ -77,7 +54,7 @@ function displayCurrentWeather(data, cityName) {
     temperatureEL.text = `Humidity: ${data.main.humidity} % `;
     currentWeatheContainerEl.appendChild(humidityEL); 
 
-    let windSpeedEL = $("<p>"); 
+    let windSpeedEl = $("<p>"); 
     temperatureEL.text = `Wind Speed: ${data.wind.speed} MPH`;
     currentWeatherContainerEL.appendChild(windSpeedEl); 
 }
@@ -86,16 +63,18 @@ function displayCurrentWeather(data, cityName) {
 function getUVindex(lat, lon) {
     let uvAPI = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}`;
 
-    fetch(uvAPI)
-    .then(function(response) {
-        response.json(); 
-        console.log(response); 
-    }).then(function(data) {
-        let index = parseFloat(data.value); 
-        displayUVIndex(index); 
-    }).catch(function(error) {"Error getting the UV index"}); 
+    $.ajax(uvAPI, {
+        dataType:"json", 
+        success: function(data) {
+            let index = parseFloat(data.value); 
+            displayUVIndex(index); 
+        },
+        error: function() {
+            alert("Error receiving the UV index")
+        }
+    })
+    
 }
-
 
 function displayUV(index) {
     let indexClass; 
@@ -113,22 +92,6 @@ function displayUV(index) {
     currentWeatherContainerEl.appendChild(UVIndexEL); 
 }
 
-function get5DayForecast(cityName) {
-    let dayAPI = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
-
-    fetch(dayAPI)
-    .then(function(response) {
-        response.json(); 
-        console.log(response);
-    }).then(function(data) {
-        display5DayForecast(data); 
-    }).catch(function(error) {"Error fetching the 5 day forecast!"})
-}
-
-function display5DayForecast(data) {
-
-}
-
 
 function searchHistory() {
     citiesListEl.innerHTML = ""; 
@@ -142,16 +105,11 @@ function searchHistory() {
 }
 
 
-function cityClickHandler(event) {
-    let cityName = event.target.textContent; 
-    getCurrentWeater(cityName); 
-}
 
 
 
-
-userFormEl.on("submit", formSubmitHandler); 
-citiesListEl.on("click", cityClickHandler); 
+userFormEl.submit(getCurrentWeather("Raleigh")); 
+//citiesListEl.click(cityClickHandler); 
 //clearBtn.click(clearSearchHistory); 
 
 searchHistory(); 
