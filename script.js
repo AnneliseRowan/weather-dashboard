b387b95fb98e22043e2a35d47c7bab23f3c5b49c
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-05 10:48:21 -0400

    added more code, found some bugs, added stuff for 5 day forecast

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ let citySearchedEL = $("#city-searched");
 let currentWeatherContainerEl = $("#current-weather-container"); 
 let forecastContainerEl = $("#forecast-container"); 
 let cities = JSON.parse(localStorage.getItem("citiesSearched")) || []; 
+let clearBtn = $("clear-btn");
 
 let citySearched; 
 
 
 const apiKey = "4994e9dae0cfd4d7106550fa9a769e96";
 
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            return false; 
+        }
+    }
+    return true; 
+}
 
 function formSubmitHandler(event) {
     event.preventDefault(); 
@@ -26,7 +35,7 @@ function formSubmitHandler(event) {
 }
 
 function cityClickHandler(event) {
-    let cityName = event.target.value(); 
+    let cityName = event.target.textContent; 
     getCurrentWeather(cityName); 
 }
 
@@ -38,7 +47,7 @@ function getCurrentWeather(cityName) {
         success: function(data) {
             console.log(data);
             displayCurrentWeather(data, cityName);
-            //get5DayForecast(cityName); 
+            get5DayForecast(cityName); 
             getUVindex(data.coord.lat, data.coord.lon)
             console.log(data.coord.lat)
             return data.coord
@@ -46,6 +55,14 @@ function getCurrentWeather(cityName) {
         error: function() {
             alert("We have a slight issue!");
             forecastContainerEl.textContent = ""; 
+            if(cities.includes(cityName)) {
+                var index = cities.indexOf(cityName);
+                if(index > -1) {
+                    cities.splice(index, 1)
+                }
+                localStorage.setItem("citiesSearched", JSON.stringify(cities));
+                searchHistory(); 
+            }
         }
     })
 
@@ -62,10 +79,49 @@ function displayCurrentWeather(data, cityName) {
         searchHistory(); 
     }
 
-   
+    if(isEmpty(data)) {
+        currentWeatherContainerEl.textContent = "No weather data has been found for this location!";
+        return; 
+    }
+
+    dtCitySearched = moment.unix(data.dt + data.timezone).utc().format("M/DD/YY, h:mm a");
+
+    citySearchedEL.append(`${cityName} ${dtCitySearched} <span id="weather-icon><img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/></span>`)
     currentWeatherContainerEl.append(`<p> Temperature: ${data.main.temp}℉ </p>`)
     currentWeatherContainerEl.append(`<p> Humidity: ${data.main.humidity}% </p>`)
     currentWeatherContainerEl.append(`<p> Wind Speed: ${data.wind.speed} MPH </p>`)
+}
+
+function get5DayForecast(cityName) {
+
+    var apiURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+
+    $.ajax(apiURL, {
+        dataType: "json",
+        success: function(data) {
+            display5DayForecast(data); 
+        },
+        error: function() {
+            //alert("Couldn't find the 5 day forecast")
+        }
+    })
+
+}
+
+function display5DayForecast(data) {
+    forecastContainerEl.append(`<h4 class="d-block pt-4 pb-2"> 5-Day Forecast <span id="time-forecast">[expected weather at noon everyday]</span><h4>`); 
+    let cardsContainerEl = ("p");
+
+
+    let firstForecast; 
+
+    let todayStartHour = moment(dtCitySearched, "M/DD/YY, h:mm a").startOf("hour").format("YYY-MM-DD HH:mm:ss"); 
+    let todayAt6Am = moment(dtCitySearched, "M/DD/YY, h:mm a").format("YYY-MM-DD") + " 06:00:00"; 
+    if(todayStartHour > todayAt6Am) {
+        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").add(1, "d").format("YYY-MM-DD") + " 12:00:00"; 
+    } else {
+        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").format("YYY-MM-DD") + " 12:00:00"
+    }
 }
 
 
@@ -102,7 +158,7 @@ function displayUVIndex(index) {
 }
 
 function searchHistory() {
-    citiesListEl.textContent = ""; 
+    citiesListEl.value = ""; 
 
     cities.forEach(function(city) {
         console.log(city)
@@ -110,10 +166,15 @@ function searchHistory() {
     })
 }
 
+function clearSearchHistory() {
+    localStorage.clear();  
+}
+
 
 
 
 userFormEl.submit(formSubmitHandler); 
 citiesListEl.click(cityClickHandler); 
+clearBtn.click(clearSearchHistory); 
 searchHistory();
 
