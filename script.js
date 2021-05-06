c3758c7815ccb3aaee1670d1a6ffe1a6a1af40f1
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-05-06 12:38:57 -0400

    got part of the 5 day forecast to show up

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function isEmpty(obj) {
 
 function formSubmitHandler(event) {
     event.preventDefault(); 
-    console.log(cityInputEl)
     let city = cityInputEl[0].value.trim();
     
     if(city) {
@@ -45,8 +44,9 @@ function getCurrentWeather(cityName) {
     $.ajax(requestUrl, {
         dataType: "json",
         success: function(data) {
+            console.log(data);
             displayCurrentWeather(data, cityName);
-            //get5DayForecast(cityName); 
+            get5DayForecast(cityName); 
             getUVindex(data.coord.lat, data.coord.lon)
             return data.coord
         },
@@ -80,7 +80,7 @@ function displayCurrentWeather(data, cityName) {
     }
 
     if(isEmpty(data)) {
-        currentWeatherContainerEl.textContent = "No weather data has been found for this location!";
+        currentWeatherContainerEl.text = "No weather data has been found for this location!";
         return; 
     }
 
@@ -90,18 +90,17 @@ function displayCurrentWeather(data, cityName) {
     currentWeatherContainerEl.append(`<p> Temperature: ${data.main.temp}℉ </p>`)
     currentWeatherContainerEl.append(`<p> Humidity: ${data.main.humidity}% </p>`)
     currentWeatherContainerEl.append(`<p> Wind Speed: ${data.wind.speed} MPH </p>`)
-
-
 }
-/*
+
 function get5DayForecast(cityName) {
 
-    var apiURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&cnt=40&appid=${apiKey}`;
 
     $.ajax(apiURL, {
         dataType: "json",
         success: function(data) {
             display5DayForecast(data); 
+            console.log(data); 
         },
         error: function() {
             //alert("Couldn't find the 5 day forecast")
@@ -112,7 +111,7 @@ function get5DayForecast(cityName) {
 
 function display5DayForecast(data) {
     forecastContainerEl.append(`<h4 class="d-block pt-4 pb-2"> 5-Day Forecast <span id="time-forecast">[expected weather at noon everyday]</span><h4>`); 
-    let cardsContainerEl = ("p");
+    let cardsContainerEl = $("<p>");
 
 
     let firstForecast; 
@@ -120,36 +119,69 @@ function display5DayForecast(data) {
     let todayStartHour = moment(dtCitySearched, "M/DD/YY, h:mm a").startOf("hour").format("YYY-MM-DD HH:mm:ss"); 
     let todayAt6Am = moment(dtCitySearched, "M/DD/YY, h:mm a").format("YYY-MM-DD") + " 06:00:00"; 
     if(todayStartHour > todayAt6Am) {
-        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").add(1, "d").format("YYY-MM-DD") + " 12:00:00"; 
+        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").add(1, "d").format("YYY-MM-DD") + "12:00:00"; 
     } else {
-        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").format("YYY-MM-DD") + " 12:00:00";
+        firstForecast = moment(dtCitySearched, "M/DD/YY, h:mm a").format("YYY-MM-DD") + "12:00:00";
     }
+
+    console.log(firstForecast); 
 
     let arrDays = data.list; 
     console.log(arrDays);
     let startIndex; 
 
+    
+    
     arrDays.forEach(function(day) {
         if(day.dt_text === firstForecast) {
-            startIndex = arrDays.indexOf(day)
-            return; 
+            console.log(startIndex)
+            return startIndex = arrDays.indexOf(day);
+            
         }
+         
     })
+    
+    console.log(startIndex); 
 
-    for(i = startIndex; i < arrDays.length; i+= 8) {
+    for(i = 6; i < arrDays.length; i+= 8) {
+        let dayForecastContainerEl = $("<div>").addClass("mx-auto");
+        let cardEl = $("<div>").addClass("card bg-primary text-white"); 
+        let cardBodyEl = $("<div>").addClass("card-body"); 
 
+        //let date = moment(arrDays[i].dt_text.split(" ")[0], "YYYY-MM-DD").format("M/DD/YY"); 
+        //console.log(date); 
+        //let dateEl = $("<h5>").addClass("card-title").text(`${date}`); 
+        //cardBodyEl.append(dateEl);
+        
+        let iconId = arrDays[i].weather[0].icon; 
+        if(iconId[iconId.length - 1] === "n") {
+            iconId = iconId.slice(0, 1) + "d"; 
+        }
+       
+        let iconEl = $("<i>").html(`<img src="https://openweathermap.org/img/wn/${iconId}.png"/>`); 
+        cardBodyEl.append(iconEl); 
+        
+        let tempEl = $("<p>").addClass("card-text").text(`Temp: ${arrDays[i].main.temp}℉`); 
+        cardBodyEl.append(tempEl); 
+
+        let humidityEl = $("<p>").addClass("card-text").text(`Humidity: ${arrDays[i].main.humidity}%`);
+        cardBodyEl.append(humidityEl); 
+
+        cardEl.append(cardBodyEl); 
+        dayForecastContainerEl.append(cardEl); 
+        cardsContainerEl.append(dayForecastContainerEl); 
     }
+
+    forecastContainerEl.append(cardsContainerEl);
 }
 
-*/
+
 function getUVindex(lat, lon) {
-    console.log(lat); 
     let uvAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     $.ajax(uvAPI, {
         dataType:"json", 
         success: function(data) {
-            console.log(data); 
             let index = parseFloat(data.current.uvi); 
             displayUVIndex(index); 
         },
@@ -177,23 +209,14 @@ function displayUVIndex(index) {
 function searchHistory() {
     citiesListEl.text = ""; 
 
-    if(cities.includes(cityName)) {
-        cities.forEach(function(city) {
-            console.log(city)
-            citiesListEl.append(`<li class="list-group-item"> ${city} </li>`);
-        })
-    }
-
-    
+    cities.forEach(function(city) {
+        citiesListEl.append(`<li class="list-group-item"> ${city} </li>`);
+    })
 }
 
 function clearSearchHistory() {
-    console.log("run"); 
     localStorage.clear();  
 }
-
-
-
 
 userFormEl.submit(formSubmitHandler); 
 citiesListEl.click(cityClickHandler); 
